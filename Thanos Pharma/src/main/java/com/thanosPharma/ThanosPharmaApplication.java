82587cf6f697ec1c83053ebb3d27b4afbd922c2d
Pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2023-03-02 17:56:21 +0100

    proyecto creado

---
+++ Thanos Pharma/src/main/java/com/thanosPharma/ThanosPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.thanosPharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThanosPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThanosPharmaApplication.class, args);
+	}
+
+}
---
+++ Thanos Pharma/src/test/java/com/thanosPharma/ThanosPharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanosPharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThanosPharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
