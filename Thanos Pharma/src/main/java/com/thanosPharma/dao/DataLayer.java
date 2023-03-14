a2952566730a0e940d77491c6ccb51d445b5af47
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2023-03-14 19:48:51 +0100

    Añadidas clases

--- Thanos Pharma/src/main/java/com/thanosPharma/dao/DataLayer.java
+++ Thanos Pharma/src/main/java/com/thanosPharma/dao/DataLayer.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.thanosPharma.logic;
+package com.thanosPharma.dao;
 
 /**
  *
  * @author Kiwi
  */
-public class NewClass {
+public class DataLayer {
     
 }
