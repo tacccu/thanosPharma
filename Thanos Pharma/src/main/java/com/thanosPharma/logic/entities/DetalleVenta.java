773bd59de9c5221a70ac913ff7e4639300993c7c
Pedro <algarra.delgado.pedro@alumnat.copernic.cat>
2023-03-14 20:08:21 +0100

    Update DetalleVenta.java

--- Thanos Pharma/src/main/java/com/thanosPharma/logic/entities/DetalleVenta.java
+++ Thanos Pharma/src/main/java/com/thanosPharma/logic/entities/DetalleVenta.java
@@ -4,10 +4,23 @@
  */
 package com.thanosPharma.logic.entities;
 
+import lombok.Data;
+
 /**
  *
  * @author Kiwi
  */
+@Data
+@Entity
+@Table(name="DetalleVenta")
 public class DetalleVenta {
+    private static final long serialVersionUID = 1l;
+    
+    private Producto producto;
+    private int idVenta;
+    private int cantidad;
+    private double precioUnitario;
+    private int iva;
+    private double totalProducto;
     
 }
