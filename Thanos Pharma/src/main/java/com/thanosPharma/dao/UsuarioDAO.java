209a77a0cba7af1e9068a785ed59798cf28299b6
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2023-03-16 16:32:36 +0100

    Añadidas interfaces DAO

--- Thanos Pharma/src/main/java/com/thanosPharma/dao/UsuarioDAO.java
+++ Thanos Pharma/src/main/java/com/thanosPharma/dao/UsuarioDAO.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.thanosPharma.logic.entities;
+package com.thanosPharma.dao;
+
+import com.thanosPharma.logic.entities.Usuario;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author Kiwi
  */
-public class DetalleCompra {
-    
+public interface UsuarioDAO extends CrudRepository<Usuario, Long> {
+
 }
