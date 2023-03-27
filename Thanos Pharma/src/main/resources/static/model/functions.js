3ac680ff569feb8579086e37918a8a2f09ceaf55
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2023-03-27 19:56:39 +0200

    Fixes visuales Añadida funcionalidad para hacer una búsqueda activa

--- Thanos Pharma/src/main/resources/static/model/functions.js
+++ Thanos Pharma/src/main/resources/static/model/functions.js
@@ -16,12 +16,14 @@ function modifyOrDeleteProducto(action) {
                 window.location.href = '/modify/' + codigoNacional;
                 break;
             case 'delete':
-                window.location.href = '/delete/' + codigoNacional;
+                if (confirm("¿Estás seguro de que quieres eliminar este registro?")) {
+                    window.location.href = '/delete/' + codigoNacional;
+                }
                 break;
         }
     } else {
 
-        alert('Por favor, seleccione un producto.');
+        alert('Por favor, seleccione un registro.');
     }
 }
 
@@ -37,3 +39,27 @@ function seleccionarUnicoCheckbox(currentCheckbox) {
         }
     }
 }
+
+function buscar() {
+    var valorBuscado = document.getElementById("cuadro-busqueda").value.toLowerCase();
+    var tablaProductos = document.getElementById("tabla-productos");
+    var filas = tablaProductos.getElementsByTagName("tr");
+    for (var i = 0; i < filas.length; i++) {
+        var celdas = filas[i].getElementsByTagName("td");
+        var filaVisible = false;
+        for (var j = 0; j < celdas.length; j++) {
+            var contenidoCelda = celdas[j].textContent.toLowerCase();
+            if (contenidoCelda.indexOf(valorBuscado) > -1) {
+                filaVisible = true;
+                break;
+            }
+        }
+        if (filaVisible) {
+            filas[i].style.display = "";
+        } else {
+            filas[i].style.display = "none";
+        }
+    }
+}
+
+
