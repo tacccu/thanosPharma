d89793cc9dcaed2f7e7ebf8c585292beb2f0c34c
Aiman Benali Kharkhour <benali.kharkhour.aiman@alumnat.copernic.cat>
2023-04-10 10:25:26 +0200

    Diseño Pantalla

--- Thanos Pharma/src/main/java/com/thanosPharma/OrdenVentaController.java
+++ Thanos Pharma/src/main/java/com/thanosPharma/OrdenVentaController.java
@@ -7,6 +7,8 @@ package com.thanosPharma;
 import com.thanosPharma.logic.services.OrdenVentaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  *
@@ -17,5 +19,10 @@ public class OrdenVentaController {
 
     @Autowired
     OrdenVentaService ordenVentaService;
+
+    @GetMapping("/mainVentas")
+    public String showMainClientes(Model model) {
+        return "mainVentas";
+    }
 
 }
