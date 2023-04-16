e9b6f091d85c347295c2a19dac27e976adfff3e2
martinez.cordoba.ricardo <martinez.cordoba.ricardo@alumnat.copernic.cat>
2023-04-16 12:23:55 +0200

    Control de errores FormProductos - Campos resaltados si hay un error en los datos introducidos.

--- Thanos Pharma/src/main/java/com/thanosPharma/logic/entities/Producto.java
+++ Thanos Pharma/src/main/java/com/thanosPharma/logic/entities/Producto.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -31,10 +30,10 @@ public class Producto implements Serializable {
     
     private boolean receta;
     
-    @Min(0)
+    @Min(value = 0, message = "El stock no puede ser negativo") 
     private int stock;
     
-    @NotNull(message = "El nombre del producto ha de tenir algún valor") 
+    @NotNull(message = "El nombre del producto ha de tener algún valor") 
     @NotEmpty(message = "El nombre del producto no puede estar vacío")  
     @Size(max = 100)
     private String nombreProducto;
